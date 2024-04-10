96eb2a86b07dfc9419bd3f2b80590554acd23d65
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-04-10 22:29:49 +0300

    Changed deploy script

--- ignition/modules/RegistrarController.ts
+++ ignition/modules/RegistrarController.ts
@@ -1,27 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { ethers, upgrades } from "hardhat";
 
 const defaultDomainPrice = 1n * 10n ** 18n; // 50_000_000_000_000_000 // 0.05 Ethers
 
-const RegistrarControllerProxyModule = buildModule("RegistrarControllerProxyModule", (m) => {
-  const proxyAdminOwner = m.getAccount(0);
+(async () => {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with account:", deployer.address);
 
-  const registrar = m.contract("RegistrarController", [proxyAdminOwner, defaultDomainPrice], { from: proxyAdminOwner });
-
-  const proxy = m.contract("TransparentUpgradeableProxy", [
-    registrar,
-    proxyAdminOwner,
-    "0x",
-  ]);
-
-  const proxyAdminAddress = m.readEventArgument(
-    proxy,
-    "AdminChanged",
-    "newAdmin"
-  );
-
-  const proxyAdmin = m.contractAt("ProxyAdmin", proxyAdminAddress);
-
-  return { proxyAdmin, proxy };
-});
-
-export default RegistrarControllerProxyModule;
+  const registrar = await ethers.getContractFactory("RegistrarController");
+  const contract = await upgrades.deployProxy(registrar, [deployer.address, defaultDomainPrice]);
+  console.log("RegistrarController deployed to:", await contract.getAddress());
+})();
