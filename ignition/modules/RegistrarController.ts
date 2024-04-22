d2f8ca34e31d3f7bbb4963740596299e5411f1db
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-04-22 23:54:53 +0300

    Added lint plugins, linted all project

--- ignition/modules/RegistrarController.ts
+++ ignition/modules/RegistrarController.ts
@@ -1,37 +1,12 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { ethers, upgrades } from "hardhat";
 
 const defaultDomainPrice = 1n * 10n ** 18n; // 50_000_000_000_000_000 // 0.05 Ethers
-
-const RegistrarControllerProxyModule = buildModule("RegistrarControllerProxyModule", (m) => {
-  const proxyAdminOwner = m.getAccount(0);
-
-  const registrar = m.contract("RegistrarController", [proxyAdminOwner, defaultDomainPrice], { from: proxyAdminOwner });
-
-  const proxy = m.contract("TransparentUpgradeableProxy", [
-    registrar,
-    proxyAdminOwner,
-    "0x",
-  ]);
-
-  const proxyAdminAddress = m.readEventArgument(
-    proxy,
-    "AdminChanged",
-    "newAdmin"
-  );
-
-  const proxyAdmin = m.contractAt("ProxyAdmin", proxyAdminAddress);
-
-  return { proxyAdmin, proxy };
-});
-
-export default RegistrarControllerProxyModule;
 
 (async () => {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contract with account:", deployer.address);
 
   const registrar = await ethers.getContractFactory("RegistrarController");
-  const contract = await upgrades.deployProxy(registrar, [deployer, defaultDomainPrice]);
+  const contract = await upgrades.deployProxy(registrar, [deployer.address, defaultDomainPrice]);
   console.log("RegistrarController deployed to:", await contract.getAddress());
 })();
--- ignition/modules/upgrade.ts
+++ ignition/modules/upgrade.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from "hardhat";
 
 (async () => {
   const registrarV2 = await ethers.getContractFactory("RegistrarController");
-  const address = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0" // Replace this. Proxy storage contract address
+  const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512" // Replace this. Proxy storage contract address
   
   const upgradedToContractV2 = await upgrades.upgradeProxy(address, registrarV2);
   console.log("RegistrarControllerV2 upgraded\n");
